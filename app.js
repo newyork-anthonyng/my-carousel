25396de8ae53e33e4505529d53025b2d6bab4076
Anthony Ng <newyork.anthonyng@gmail.com>
2019-01-04 18:34:24 -0500

    Add styling to animate moving image

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("app.js loaded");
+
+const carouselInner = document.querySelector(".js-carousel-inner");
+const carouselImages = document.querySelectorAll(".js-carousel-img");
+
+setTimeout(() => {
+    carouselImages[0].style.transform = `translateX(-100%)`;
+}, 1000);
+
+// let index = 0;
+// setInterval(() => {
+//     if (index >= carouselImages.length) {
+//         return;
+//     }
+// 
+//     const currentImage = carouselImages[index];
+//     const nextImage = carouselImages[index + 1];
+// 
+//     // move currentImage off view
+//     currentImage.style.transform = `translateX(-${100 * index}%)`;
+// 
+//     // move nextImage out of view
+//     nextImage.style.transform = `translateX(-${100 * index}%)`;
+// 
+//     index++;
+// }, 1000);
