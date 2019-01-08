7baf7274e89d47cec8415141522ebdeafa763623
Anthony Ng <newyork.anthonyng@gmail.com>
2019-01-08 07:16:49 -0500

    Handle forward moving carousel

--- app.js
+++ app.js
@@ -1,26 +1,78 @@
-console.log("app.js loaded");
+const images = document.querySelectorAll(".js-carousel-img");
+const nextButton = document.querySelector(".js-carousel-next");
 
-const carouselInner = document.querySelector(".js-carousel-inner");
-const carouselImages = document.querySelectorAll(".js-carousel-img");
+nextButton.addEventListener("click", handleNextButton);
 
-setTimeout(() => {
-    carouselImages[0].style.transform = `translateX(-100%)`;
-}, 1000);
+let currentImageIndex = 0;
 
-// let index = 0;
-// setInterval(() => {
-//     if (index >= carouselImages.length) {
-//         return;
-//     }
-// 
-//     const currentImage = carouselImages[index];
-//     const nextImage = carouselImages[index + 1];
-// 
-//     // move currentImage off view
-//     currentImage.style.transform = `translateX(-${100 * index}%)`;
-// 
-//     // move nextImage out of view
-//     nextImage.style.transform = `translateX(-${100 * index}%)`;
-// 
-//     index++;
-// }, 1000);
+function handleNextButton() {
+    let nextImageIndex = currentImageIndex + 1;
+    if (nextImageIndex === images.length) {
+        nextImageIndex = 0;
+    }
+
+    let nextNextImageIndex = nextImageIndex + 1;
+    if (nextNextImageIndex === images.length) {
+        nextNextImageIndex = 0;
+    }
+
+    moveLeft(images[currentImageIndex]);
+    moveLeft(images[nextImageIndex]);
+
+    // prepare next image to come into the carousel
+    moveToRightSideOfCarousel(images[nextNextImageIndex], nextNextImageIndex);
+
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+}
+
+function moveToRightSideOfCarousel(node, index) {
+    if (!node) {
+        return;
+    }
+
+    let startingXOffset = 0;
+    if (index === 0) {
+        startingXOffset = 100;
+    } else {
+        startingXOffset = (index - 1) * 100 * -1;
+    }
+
+    node.style.opacity = "0";
+    node.style.transform = `translateX(${startingXOffset}%)`;
+    node.addEventListener("transitionend", handleTransitionEnd)
+}
+
+function handleTransitionEnd() {
+    this.removeEventListener("transitionend", handleTransitionEnd);
+    this.style.opacity = "1";
+}
+
+function moveLeft(node) {
+    if (!node) {
+        return;
+    }
+
+    const currentTransform = getTransform(node);
+    const nextTransform = currentTransform - 100;
+
+    node.style.transform = `translateX(${nextTransform}%)`;
+}
+
+const TRANSLATE_X_REGEX = /.*\((-?\d*)%\)/;
+function getTransform(node) {
+    if (!node) {
+        return;
+    }
+
+    const { transform } = node.style;
+
+    if (transform === "") {
+        return 0;
+    }
+
+    const parsedTransform = TRANSLATE_X_REGEX.exec(transform);
+    return +parsedTransform[1];
+}
