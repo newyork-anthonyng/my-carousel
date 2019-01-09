694538d77c0e29fbcf763fd5c1ec031391780950
Anthony Ng <newyork.anthonyng@gmail.com>
2019-01-08 20:03:03 -0500

    Add previous button

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const images = document.querySelectorAll(".js-carousel-img");
 const nextButton = document.querySelector(".js-carousel-next");
-
-nextButton.addEventListener("click", handleNextButton);
+const previousButton = document.querySelector(".js-carousel-previous");
+const carouselIndex = document.querySelector(".js-carousel-index");
 
 let currentImageIndex = 0;
+
+nextButton.addEventListener("click", () => {
+    handleNextButton();
+    renderCurrentImageIndex();
+});
+
+// TODO: need to handle if user presses on previousButton first
+previousButton.addEventListener("click", () => {
+    handlePreviousButton();
+    renderCurrentImageIndex();
+});
+
+function renderCurrentImageIndex() {
+    carouselIndex.textContent = currentImageIndex;
+}
 
 function handleNextButton() {
     let nextImageIndex = currentImageIndex + 1;
@@ -28,6 +43,40 @@ function handleNextButton() {
     }
 }
 
+function handlePreviousButton() {
+    let nextImageIndex = currentImageIndex - 1;
+    if (nextImageIndex < 0) {
+        nextImageIndex = images.length - 1;
+    }
+
+    let nextNextImageIndex = nextImageIndex - 1;
+    if (nextNextImageIndex < 0) {
+        nextNextImageIndex = images.length - 1;
+    }
+
+    moveRight(images[currentImageIndex]);
+    moveRight(images[nextImageIndex]);
+
+    moveToLeftSideOfCarousel(images[nextNextImageIndex], nextNextImageIndex);
+
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+}
+
+function moveToLeftSideOfCarousel(node, index) {
+    if (!node) {
+        return;
+    }
+
+    const startingXOffset =  (index + 1) * 100 * -1;
+
+    node.style.opacity = "0";
+    node.style.transform = `translateX(${startingXOffset}%)`;
+    node.addEventListener("transitionend", handleTransitionEnd)
+}
+
 function moveToRightSideOfCarousel(node, index) {
     if (!node) {
         return;
@@ -48,6 +97,17 @@ function moveToRightSideOfCarousel(node, index) {
 function handleTransitionEnd() {
     this.removeEventListener("transitionend", handleTransitionEnd);
     this.style.opacity = "1";
+}
+
+function moveRight(node) {
+    if (!node) {
+        return;
+    }
+
+    const currentTransform = getTransform(node);
+    const nextTransform = currentTransform + 100;
+
+    node.style.transform = `translateX(${nextTransform}%)`;
 }
 
 function moveLeft(node) {
