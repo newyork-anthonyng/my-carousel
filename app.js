3fa2a3750a3a78ba159967bd1ef6d65e84e5b47c
Anthony Ng <newyork.anthonyng@gmail.com>
2019-01-17 21:39:03 -0500

    It works!

--- app.js
+++ app.js
@@ -1,97 +1,58 @@
-const images = document.querySelectorAll(".js-carousel-img");
-const nextButton = document.querySelector(".js-carousel-next");
-const previousButton = document.querySelector(".js-carousel-previous");
-const carouselIndex = document.querySelector(".js-carousel-index");
+const previousButtonEle = document.querySelector(".js-carousel-previous");
+const nextButtonEle = document.querySelector(".js-carousel-next");
+const carouselInnerEle = document.querySelector(".js-carousel-inner");
 
-let currentImageIndex = 0;
-
-nextButton.addEventListener("click", () => {
-    handleNextButton();
-    renderCurrentImageIndex();
+// duplicate all images
+const originalImages = document.querySelectorAll(".js-carousel-img");
+originalImages.forEach((image, index) => {
+    image.setAttribute("data-index", index);
+});
+const duplicatedImages = [...originalImages].map(image => {
+    return image.cloneNode(true);
+});
+duplicatedImages.forEach((image, index) => {
+    image.setAttribute("data-index", index + 3);
+    carouselInnerEle.appendChild(image);
 });
 
-// TODO: need to handle if user presses on previousButton first
-previousButton.addEventListener("click", () => {
-    handlePreviousButton();
-    renderCurrentImageIndex();
-});
+const OFFSET = -50;
+const leftOfCarouselTranslates = [
+    -100 + OFFSET,
+    -200 + OFFSET,
+    -300 + OFFSET,
+    -400 + OFFSET,
+    -500 + OFFSET,
+    -600 + OFFSET
+];
 
-function renderCurrentImageIndex() {
-    carouselIndex.textContent = currentImageIndex;
-}
+function moveToLeftOfCarousel(node) {
+    const index = node.dataset.index;
 
-function handleNextButton() {
-    let nextImageIndex = currentImageIndex + 1;
-    if (nextImageIndex === images.length) {
-        nextImageIndex = 0;
+    const isOnRightOfCarousel = getTransform(node) === leftOfCarouselTranslates[index] + 400;
+    console.log("is on right of carousel", isOnRightOfCarousel);
+    if (isOnRightOfCarousel) {
+        node.style.opacity = "0";
     }
+    translateX(node, leftOfCarouselTranslates[index]);
 
-    let nextNextImageIndex = nextImageIndex + 1;
-    if (nextNextImageIndex === images.length) {
-        nextNextImageIndex = 0;
-    }
-
-    moveLeft(images[currentImageIndex]);
-    moveLeft(images[nextImageIndex]);
-
-    // prepare next image to come into the carousel
-    moveToRightSideOfCarousel(images[nextNextImageIndex], nextNextImageIndex);
-
-    currentImageIndex++;
-    if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
+    if (isOnRightOfCarousel) {
+        node.addEventListener("transitionend", handleTransitionEnd);
     }
 }
 
-function handlePreviousButton() {
-    let nextImageIndex = currentImageIndex - 1;
-    if (nextImageIndex < 0) {
-        nextImageIndex = images.length - 1;
+function moveToRightOfCarousel(node) {
+    const index = node.dataset.index;
+
+    const isOnLeftOfCarousel = leftOfCarouselTranslates[index] === getTransform(node);
+    if (isOnLeftOfCarousel) {
+        node.style.opacity = "0";
+        
     }
+    translateX(node, leftOfCarouselTranslates[index] + 400);
 
-    let nextNextImageIndex = nextImageIndex - 1;
-    if (nextNextImageIndex < 0) {
-        nextNextImageIndex = images.length - 1;
+    if (isOnLeftOfCarousel) {
+        node.addEventListener("transitionend", handleTransitionEnd);
     }
-
-    moveRight(images[currentImageIndex]);
-    moveRight(images[nextImageIndex]);
-
-    moveToLeftSideOfCarousel(images[nextNextImageIndex], nextNextImageIndex);
-
-    currentImageIndex--;
-    if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-    }
-}
-
-function moveToLeftSideOfCarousel(node, index) {
-    if (!node) {
-        return;
-    }
-
-    const startingXOffset =  (index + 1) * 100 * -1;
-
-    node.style.opacity = "0";
-    node.style.transform = `translateX(${startingXOffset}%)`;
-    node.addEventListener("transitionend", handleTransitionEnd)
-}
-
-function moveToRightSideOfCarousel(node, index) {
-    if (!node) {
-        return;
-    }
-
-    let startingXOffset = 0;
-    if (index === 0) {
-        startingXOffset = 100;
-    } else {
-        startingXOffset = (index - 1) * 100 * -1;
-    }
-
-    node.style.opacity = "0";
-    node.style.transform = `translateX(${startingXOffset}%)`;
-    node.addEventListener("transitionend", handleTransitionEnd)
 }
 
 function handleTransitionEnd() {
@@ -99,27 +60,76 @@ function handleTransitionEnd() {
     this.style.opacity = "1";
 }
 
-function moveRight(node) {
-    if (!node) {
-        return;
-    }
-
-    const currentTransform = getTransform(node);
-    const nextTransform = currentTransform + 100;
-
-    node.style.transform = `translateX(${nextTransform}%)`;
+function moveToLeftMost(node) {
+    const index = node.dataset.index;
+    translateX(node, leftOfCarouselTranslates[index] + 100);
 }
 
-function moveLeft(node) {
-    if (!node) {
-        return;
-    }
-
-    const currentTransform = getTransform(node);
-    const nextTransform = currentTransform - 100;
-
-    node.style.transform = `translateX(${nextTransform}%)`;
+function moveToCenter(node) {
+    const index = node.dataset.index;
+    translateX(node, leftOfCarouselTranslates[index] + 200);
 }
+
+function moveToRightMost(node) {
+    const index = node.dataset.index;
+    translateX(node, leftOfCarouselTranslates[index] + 300);
+}
+
+function translateX(node, amount) {
+    node.style.transform = `translateX(${amount}%)`;
+}
+
+/*
+ [ ] represents the visible part of the carousel
+ 3 [ 1 2 3 ] 1 2
+ go next
+ 1 [ 2 3 1 ] 2 3
+*/
+let images = [
+    duplicatedImages[2],
+    ...originalImages,
+    duplicatedImages[0],
+    duplicatedImages[1]
+];
+
+function positionImages(images) {
+    const [
+        leftSideOfCarousel,
+        leftmostImage,
+        centerImage,
+        rightmostImage,
+        rightSideOfCarousel,
+        anotherRightSideOfCarousel
+    ] = images;
+
+    moveToLeftOfCarousel(leftSideOfCarousel);
+    moveToLeftMost(leftmostImage)
+    moveToCenter(centerImage)
+    moveToRightMost(rightmostImage)
+    moveToRightOfCarousel(rightSideOfCarousel)
+    moveToRightOfCarousel(anotherRightSideOfCarousel);
+}
+
+positionImages(images);
+
+nextButtonEle.addEventListener("click", () => {
+    const newImages = [
+        ...images.slice(1),
+        images[0]
+    ];
+    images = newImages;
+    positionImages(newImages);
+});
+
+previousButtonEle.addEventListener("click", () => {
+    const newImages = [
+        images[images.length - 1],
+        ...images.slice(0, images.length - 1),
+        
+    ];
+    images = newImages;
+    positionImages(newImages);
+});
 
 const TRANSLATE_X_REGEX = /.*\((-?\d*)%\)/;
 function getTransform(node) {
